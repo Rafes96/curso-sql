8807df52a8d2868be509b82b3b15f638700e9020
Rafael Araújo <rafael.araujo@stand.com.br>
2022-08-20 17:19:28 -0300

    Inserido dado na tabela funcionarios

---
+++ inserindo campo na tabela.sql
@@ -0,0 +1,12 @@
+--importando um arquivo 
+
+CREATE DATABASE empresa
+
+use empresa
+
+select * from funcionarios
+select * from servicos
+
+insert into funcionarios values ('Rafael', 'Dev .Net')
+
+update funcionarios set idade = '25' where id = 5

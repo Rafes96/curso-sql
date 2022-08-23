48a041f1ba27e6360c36665dc389c1b8a41ab999
Rafael Araújo <rafael.araujo@stand.com.br>
2022-08-22 23:34:53 -0300

    Aula de insert sql

---
+++ Tabelas.sql
@@ -0,0 +1,51 @@
+use empresa
+
+create database teste_tipo_dados
+
+use teste_tipo_dados
+
+
+
+--create table Pessoas(
+--nome varchar(255), 
+--cpf char(11),
+--descricao nvarchar,
+
+--)
+
+--select * from pessoas 
+
+
+--create table Produtos(
+--nome varchar(50),
+--sku char(5),
+--informacoes TEXT,
+--preco numeric(18,2)
+--)
+----drop table Produtos
+--select * from Produtos
+
+--insert into Produtos values ('rafael', '12345','TTEST TAMANHO GRANDE PRA FVER SE VAI', 2.99)
+
+--CREATE TABLE CADASTRO(
+--NOME VARCHAR(100),
+--SOBRENOME VARCHAR(100))
+
+
+--INSERT INTO CADASTRO VALUES('Rafael', 'Ferreira de Araújo')
+
+--SELECT * FROM CADASTRO
+
+
+--CREATE TABLE SERVIDORES (
+--NOME VARCHAR(100),
+--ESPACO_DISCO INT,
+--LIGADO BIT
+--)
+
+
+--INSERT INTO SERVIDORES VALUES ('SERVIDOR TESTE', 1000, 1)
+--INSERT INTO SERVIDORES VALUES ('SERVIDOR TESTE', 1000, 0)
+
+
+--SELECT * FROM SERVIDORES

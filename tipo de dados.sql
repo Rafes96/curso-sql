f08dd333abecf30a940ae2e5a3340be4751174f1
Rafael Araújo <rafael.araujo@stand.com.br>
2022-08-24 23:26:30 -0300

    Adicionando comentário no arquivo

--- tipo de dados.sql
+++ tipo de dados.sql
@@ -8,6 +8,7 @@ DataCadastro date
 
 insert into DataNascimento values('Rafael Ferreira de Araujo','1996-16-10 23:59:59' , '2022-08-24')
 
+-- datetime precisa invertar na data dia e ano, já o date não é necessário
 
 
 select * from DataNascimento

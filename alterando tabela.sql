886d9438cb4614b818d89673a8695051923625ba
Rafael Araújo <rafael.araujo@stand.com.br>
2022-08-27 23:12:08 -0300

    Alteração de tabelas

--- alterando tabela.sql
+++ alterando tabela.sql
@@ -6,3 +6,13 @@ select * from Funcionarios
 alter table Funcionarios add  Profissao varchar (255)
 
 select * from Funcionarios
+
+
+alter table funcionarios drop column Profissao
+
+alter table  Funcionarios add Nascimento varchar(100)
+
+
+select * from Funcionarios
+
+alter table funcionarios alter column nascimento Date
